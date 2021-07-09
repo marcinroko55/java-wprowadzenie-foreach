cd937583b99925bcedc96d35dfff444b3ec2a336
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-09 18:51:02 +0200

    #1 rozwiązanie zdania

---
+++ src/newPrices.java
@@ -0,0 +1,14 @@
+public class newPrices {
+    public static void main(String[] args) {
+        double[] prices = {10.5, 12, 15, 56, 9};
+
+
+        for (double price : prices) {
+
+
+
+            System.out.println("Cena powiększona o 10% wynosi: " + price*1.1);
+
+        }
+    }
+}
